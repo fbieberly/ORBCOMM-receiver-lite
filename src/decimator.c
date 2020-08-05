8c556885dec5795ea1a3ab316f5bd17bdf25da92
Frank Bieberly <frank.bieberly@gmail.com>
2020-08-05 09:30:03 -0500

    adding decimator program

--- src/decimator.c
+++ src/decimator.c
@@ -1,4 +1,4 @@
-// gcc -g -o ./decimator ./decimator.c -lm -lliquid
+// gcc -g -o ./decimator ./decimator.c -lm -lliquid -O2
 
 #include <stdio.h>
 #include <unistd.h>
@@ -15,7 +15,12 @@
 
 int main(void)
 {
-    int chunk_size = 4096*32;
+
+    FILE *out_file = NULL;
+    // char out_filename[] = "./out_samps.dat";
+    // out_file = fopen(out_filename, "wb");
+
+    int chunk_size = 250 * 96;
     uint8_t *data_buffer = (uint8_t *) malloc(chunk_size);
 
     // decimating LPF parameters
@@ -26,7 +31,7 @@ int main(void)
     float lpf_h[lpf_h_len];
 
     // create taps for fir low pass filter
-    liquid_firdes_kaiser(lpf_h_len, fc, As, mu, lpf_h);
+    liquid_firdes_kaiser(lpf_h_len, ft, As, mu, lpf_h);
 
     // create liquid decimating low pass filter object
     firdecim_crcf decim_lpf_q = firdecim_crcf_create(DECIM, lpf_h, lpf_h_len);
@@ -36,27 +41,35 @@ int main(void)
     float complex samples[chunk_size/2];
     float complex decim_samples[chunk_size/4];
 
+    // sleep(1);
+
     while(1)
     {
         // Read samples in from STDIN
         int nsamps = read(STDIN_FILENO, data_buffer, chunk_size);
-        if (nsamps < 10){ break; }
+        // if (nsamps < chunk_size){ break; }
 
         // Convert uint8_t samples from RTLSDR to complex floats
-        for (int j = 0; j < chunk_size/2; ++j)
+        for (int j = 0; j < nsamps/2; ++j)
         {
             float real = (float) data_buffer[2*j]/127.5 - 1.0;
             float imag = (float) data_buffer[2*j+1]/127.5 - 1.0;
             samples[j] = real + imag*_Complex_I;
         }
+        // fwrite(samples, sizeof(samples), 1, out_file);
+
 
         // Low pass filter and decimate by 2
-        firdecim_crcf_execute_block(decim_lpf_q, samples, chunk_size/2/DECIM, decim_samples);
+        firdecim_crcf_execute_block(decim_lpf_q, samples, nsamps/2/DECIM, decim_samples);
+
+        // write samples to file for debugging
+        // fwrite(decim_samples, sizeof(decim_samples), 1, out_file);
 
         // write out samples to STDOUT
-        fwrite(decim_samples, sizeof(decim_samples), 1, stdout);
+        fwrite(decim_samples, nsamps/2/DECIM*8, 1, stdout);
     }
 
+    // fclose(out_file);
     free(data_buffer);
     return 0;
 }
